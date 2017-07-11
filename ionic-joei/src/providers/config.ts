1854fc4871d0d23e1665b50e4245f433e479b1a2
Mark Terence <markterencetiglao@gmail.com>
2017-07-11 19:01:35 +0800

    renamed `constants.ts` to `config.ts`, changed `const` to `let`.

---
+++ ionic-joei/src/providers/config.ts
@@ -0,0 +1,6 @@
+export let SERVER_URL = 'http://192.168.1.23/api/';
+export let PROXY_SERVER = 'http://192.168.1.139/proxy-joei/proxy.php?url=%s';
+
+export let PROJECT_ENDPOINT = SERVER_URL + 'projects/';
+//TEST
+export let TEST_GET = 'http://localhost/mock/mock/description.txt';
