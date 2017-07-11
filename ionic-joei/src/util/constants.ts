3c510bfd6ade23f60161b861769feff7d4658884
Mark Terence <markterencetiglao@gmail.com>
2017-07-11 19:02:34 +0800

    renamed to `config.ts`

--- ionic-joei/src/util/constants.ts
+++
@@ -1,6 +0,0 @@
-export const SERVER_URL = 'http://192.168.1.23/api/';
-export const PROXY_SERVER = 'http://192.168.1.139/proxy-joei/proxy.php?url=%s';
-
-export const PROJECT_ENDPOINT = SERVER_URL + 'projects/';
-//TEST
-export const TEST_GET = 'http://localhost/mock/mock/description.txt';
