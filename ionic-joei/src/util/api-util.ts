2fd4ac187670b6af7ab3edfe898ea8ab869fd188
Mark Terence <markterencetiglao@gmail.com>
2017-07-12 12:49:49 +0800

    normalizeParameter and makeDataUrl

--- ionic-joei/src/util/api-util.ts
+++ ionic-joei/src/util/api-util.ts
@@ -28,72 +28,66 @@ export class ApiUtils{
     {   
         if(this.useProxy) Proxyfi(url);
 
-        //create an empty object if requiredParams or optionalParams is empty
-        if(typeof(optionalParams) === 'undefined') optionalParams = {};
-        if(typeof(requiredParams) === 'undefined') requiredParams = {};
-
-        let parameter = Object.assign(optionalParams, requiredParams);
+        let parameter = this.normalizeParameters(optionalParams, requiredParams);
 
         let params = new URLSearchParams();
         for(var key in parameter){
             params.set(key, parameter[key]);
         }
+
         return this.http.get(url, params.toString()).map(resp => resp.json());
-    
     }
 
     //Post Request
     public Post(url, requiredParams, optionalParams?){
         if(this.useProxy) Proxyfi(url);
-        
-        if(typeof(optionalParams) === 'undefined') optionalParams = {};
-        if(typeof(requiredParams) === 'undefined') requiredParams = {};
 
-        let parameter = Object.assign(optionalParams, requiredParams);
+        let parameter = this.normalizeParameters(optionalParams, requiredParams);
 
-        //Escape data
         if(parameter !== {})
-            url = url + '?' + this.transformRequest(parameter);
+            url = this.makeDataUrl(url, parameter);
 
         //TODO: header
-
         return this.http.post(url, parameter).map(resp => resp.json());
     }
 
     public Put(url, requiredParams, optionalParams?){
         if(this.useProxy) Proxyfi(url);
 
-        if(typeof(optionalParams) === 'undefined') optionalParams = {};
-        if(typeof(requiredParams) === 'undefined') requiredParams = {};
-
-        let parameter = Object.assign(optionalParams, requiredParams);
+        let parameter = this.normalizeParameters(optionalParams, requiredParams);
         
-        //Escape data
         if(parameter !== {})
-            url = url + '?' + this.transformRequest(parameter);
+            url = this.makeDataUrl(url, parameter);
 
         //TODO: header
-
         return this.http.put(url, parameter).map(resp => resp.json());
     }
 
     public Delete(url, requiredParams, optionalParams?){
         if(this.useProxy) Proxyfi(url);
 
-        if(typeof(optionalParams) === 'undefined') optionalParams = {};
-        if(typeof(requiredParams) === 'undefined') requiredParams = {};
+        let parameter = this.normalizeParameters(optionalParams, requiredParams);
 
-        let parameter = Object.assign(optionalParams, requiredParams);
-        
-        //Escape data
         if(parameter !== {})
-            url = url + '?' + this.transformRequest(parameter);
+            url = this.makeDataUrl(url, parameter);
 
         //TODO: header
-
         return this.http.delete(url, parameter).map(resp => resp.json());
     }
 
+    // Normalize a parameters into object
+    private normalizeParameters(requiredParams, optionalParams){
+        if(typeof(optionalParams) === 'undefined') optionalParams = {};
+        if(typeof(requiredParams) === 'undefined') requiredParams = {};
+        return Object.assign(optionalParams, requiredParams);
+    }
+
+    //Combine parameter to endpoint url
+    private makeDataUrl(url, parameter){
+        return url + "?" + this.transformRequest(parameter);
+    }
+
+    //Escapes special characters
     public escapeSpecialCharacters(string) {
         var tmp = encodeURIComponent(string);
         tmp = tmp.replace(/\!/g, "%21");
@@ -104,6 +98,7 @@ export class ApiUtils{
         return tmp;
     }
 
+    // Tranforms object into request url format
     public transformRequest(obj) {
         var str = [];
         for(var p in obj)
