d3ca6e8f2c6d734d88b7e1c30369b73253f3f9d6
Mark Terence <markterencetiglao@gmail.com>
2017-08-22 11:17:55 +0800

    changed to localhost

--- ionic-joei/src/providers/config.ts
+++ ionic-joei/src/providers/config.ts
@@ -1,3 +1,3 @@
 
-export let SERVER_URL = 'http://192.168.1.23:8888/api/';
+export let SERVER_URL = 'http://localhost:8100/api/';
 export let PROXY_SERVER = 'http://192.168.1.139/proxy-joei/proxy.php?url=%s';
